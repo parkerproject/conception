344f86463f70c7e1fb98f90d2ce73ac46887b3bd
Parker <parkerproject@gmail.com>
2014-07-29 02:02:59 -0400

    updates

--- routes/artist_login.js
+++ routes/artist_login.js
@@ -28,24 +28,28 @@ passport.deserializeUser(function(user, done) {
 passport.use(new LocalStrategy(function(username, password, done) {
     // asynchronous verification, for effect...
     process.nextTick(function() {
-        db.artists.findOne({
-            email: username
-        }, function(err, user) {
-            if (err) {
-                return done(err);
-            }
-            if (!user) {
-                return done(null, false, {
-                    message: 'Unknown username ' + username
+        passport.use(new LocalStrategy(
+            function(username, password, done) {
+                db.artists.findOne({
+                    email: username
+                }, function(err, user) {
+                    if (err) {
+                        return done(err);
+                    }
+                    if (!user) {
+                        return done(null, false, {
+                            message: 'Incorrect username.'
+                        });
+                    }
+                    if (!user.validPassword(password)) {
+                        return done(null, false, {
+                            message: 'Incorrect password.'
+                        });
+                    }
+                    return done(null, user);
                 });
             }
-            if (user.password !== password) {
-                return done(null, false, {
-                    message: 'Invalid password'
-                });
-            }
-            return done(null, user);
-        });
+        ));
     });
 }));
 
