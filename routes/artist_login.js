1c2d5f3557aa3fb54c2760c5f94bbfe3aed5aa7c
parkerproject <parkerproject@gmail.com>
2014-07-29 17:36:16 +0000

    updates

--- routes/artist_login.js
+++ routes/artist_login.js
@@ -82,7 +82,7 @@ module.exports = function(router) {
   });
 
 
-  router.post('/artist/update', function(req, res) {
+  router.post('/artist_update', function(req, res) {
 		
 		console.log(req.body);
 
