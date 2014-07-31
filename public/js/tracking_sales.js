7af1456415ef2523480125a769817ba4c87f4779
parkerproject <parkerproject@gmail.com>
2014-07-31 14:56:37 +0000

    up

--- public/js/tracking_sales.js
+++ public/js/tracking_sales.js
@@ -1,20 +1,21 @@
 /**
-* Created with conception.
-* User: parkerproject
-* Date: 2014-07-31
-* Time: 04:18 AM
-* To change this template use Tools | Templates.
-*/
+ * Created with conception.
+ * User: parkerproject
+ * Date: 2014-07-31
+ * Time: 04:18 AM
+ * To change this template use Tools | Templates.
+ */
 
 
 
-(function($){
-	
-   var sendTo = "http://www.eventbrite.com/event/4653432542?ref=ebtn";
-	
-	$('.artist-profile').on('click', 'button', function(e){
-		e.preventDefault();
-		var user = $(this).attr('data-artist');
-		console.log(user, $.cookie('conception_event'));
-	});
+(function($) {
+
+  var sendTo = "http://www.eventbrite.com/event/4653432542?ref=ebtn";
+
+  $('.artist-profile').on('click', 'button', function(e) {
+    e.preventDefault();
+    var user = $(this).attr('data-artist');
+    var event_id = $.cookie('conception_event');
+    window.location.href = 'http://www.eventbrite.com/event/' + event_id + '?ref=' + user;
+  });
 })(jQuery);
