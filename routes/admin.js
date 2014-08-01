878c796981deb30f797a87600b29a1103b8dd05d
parkerproject <parkerproject@gmail.com>
2014-08-01 13:19:45 +0000

    updates

--- routes/admin.js
+++ routes/admin.js
@@ -5,9 +5,10 @@
  * Time: 02:48 AM
  * To change this template use Tools | Templates.
  */
+var getEvents = require('../models/get_events');
 
 function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
+  if (req.session && req.session.authenticated) {
     return next();
   }
   res.redirect('/admin');
@@ -30,18 +31,17 @@ module.exports = function(router, passport, db) {
       username: req.body.username,
       password: req.body.password
     }, function(err, user) {
-		
+
       if (err || !user) res.redirect('/admin');
-      else {
-				req.session.user = user;
+        req.session.authenticated = true;
         res.redirect('/conception');
-      }
+      
     });
 
   });
 
 
-  router.get('/conception/:name', function(req, res) {
+  router.get('/conception/:name', ensureAuthenticated, function(req, res) {
     getEvents(function(data) {
       res.render('admin/home', {
         title: 'Conception',
@@ -50,14 +50,15 @@ module.exports = function(router, passport, db) {
     });
   });
 
-  router.get('/conception', function(req, res) {
-console.log(req.session.user);
-    getEvents(function(data) {
-      res.render('admin/home', {
-        title: 'Conception',
-        data: data
+  router.get('/conception', ensureAuthenticated, function(req, res) {
+
+      getEvents(function(data) {
+        res.render('admin/home', {
+          title: 'Conception',
+          data: data
+        });
       });
-    });
+   
   });
 
 
