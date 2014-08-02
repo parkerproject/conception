b13dea668035cf3d47f1bcb378b5c4a3719b88e2
parkerproject <parkerproject@gmail.com>
2014-08-02 23:23:13 +0000

    updates

--- public/conception/js/app.js
+++ public/conception/js/app.js
@@ -47,12 +47,48 @@ var CONCEPTION = (function() {
     return html;
   }
 
+  function artistTplHeader(row) {
+    var html = ['<div class="box">',
+      '<div class="box-body">',
+      '<table class="table table-bordered table table-hover dataTable">',
+      '<tbody><tr>',
+      '<th style="width: 10px">Name</th>',
+      '<th>Email</th>',
+      '<th>Date of Birth</th>',
+      '<th>Artwork</th>',
+      '<th>Url</th>',
+      '<th>Approved</th>',
+      '</tr>' + row + '</tbody></table>',
+      '</div>',
+      '</div>'
+    ].join("");
+    return html;
+  }
+
+  function artistTemplate(artist) {
+
+    var colorLabel, html, ticketSold = 0;
+    html = ['<tr>',
+      '<td>' + artist.full_name + '</td>',
+      '<td>' + artist.email + '</td>',
+      '<td>' + artist.date_birth + '</td>',
+      '<td>' + artist.artwork + '</td>',
+      '<td>' + artist.url + '</td>',
+      '<td width="10%"><span class="badge ' + colorLabel + '">' + ticketSold + '</span></td>',
+      '</tr>'
+    ].join("");
+
+    return html;
+
+  }
+
 
   function routes() {
 
     page('/conception/:name', function(ctx) {
 
       var name = ctx.params.name;
+			
       if (name == 'events') {
         $.getJSON('/conception/' + name, function(data) {
           var events = data.events,
@@ -65,6 +101,22 @@ var CONCEPTION = (function() {
           rows = template(html.join(""));
           document.querySelector('.event_json').innerHTML = rows;
           document.querySelector('.content-header').querySelector('h1').innerHTML = 'Events';
+        });
+      }
+
+      if (name == 'artists') {
+        $.getJSON('/conception/' + name, function(data) {
+					window.artists = data;
+//           var events = data.events,
+//             html = [],
+//             rows;
+//           for (var i = 0; i < events.length; i++) {
+//             html.push(rowTpl(events[i].event));
+//           }
+
+//           rows = template(html.join(""));
+//           document.querySelector('.event_json').innerHTML = rows;
+//           document.querySelector('.content-header').querySelector('h1').innerHTML = 'Events';
         });
       }
 
