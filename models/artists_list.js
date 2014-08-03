0004e36039de8f1df415544f9418a395c4c564b0
parkerproject <parkerproject@gmail.com>
2014-08-03 00:00:19 +0000

    updates

--- models/artists_list.js
+++ models/artists_list.js
@@ -2,10 +2,10 @@ var db = require('../config/database.js');
 
 module.exports = exports = function(fn) {
   var artists = [];
-  db.artists.find({}, function(err, artist) {
-    if (err || !events) console.log("No artist users found");
+  db.artists.find({}, function(err, result) {
+    if (err || !result) console.log("No artist users found");
 
-    else events.forEach(function(artist) {
+    else result.forEach(function(artist) {
       artists.push(artist);
     });
 
