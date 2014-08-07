729491f0c9aef3ce1923c80a5dc6c0f2db0211cc
Parker <parkerproject@gmail.com>
2014-08-06 23:19:04 -0400

    updates

--- routes/event.js
+++ routes/event.js
@@ -2,15 +2,13 @@ require('dotenv').load();
 
 var db = require('../config/database');
 
-function getArtist(email, fn) {
-  db.artists.findOne({
-    email: email,
-    emailVerified: true,
-    approved: true
-  }, function(err, user) {
-    if (err || !user) console.log("No user found");
-    else fn(user);
-  });
+function getEvent(id, fn) {
+    db.events.findOne({
+        event_id: id
+    }, function(err, event) {
+        if (err || !event) console.log(err);
+        else fn(event);
+    });
 }
 
 
@@ -18,41 +16,29 @@ function getArtist(email, fn) {
 module.exports = function(router) {
 
 
-  router.get('/event/:id', function(req, res) {
-    var id = parseInt(req.params.id);
-    db.events.findOne({
-      "event_id": id
-    }, function(err, event) {
-      if (err || !event) {
-        console.log(err);
-      } else {
+    router.get('/event/:id', function(req, res) {
+        var id = parseInt(req.params.id);
+        db.artists.find({
+            "events": id
+        }, function(err, artists) {
+            if (err || !artists) {
+                console.log(err);
+            } else {
+                getEvent(id, function(event) {
+                    res.render('event', {
+                        title: 'conception events',
+                        data: JSON.stringify(event),
+                        artists: artists,
+                        event_id: id
+                    });
+                })
 
-        var artists = [];
 
-        event.artists.map(function(artist) {
-          getArtist(artist, function(details) {
-						artists.push({
-							user_token: details.user_token,
-							photo: details.photo,
-							full_name: details.full_name,
-							genre: details.genre
-						});
-          });
+            }
         });
-        setTimeout(function() {
-          res.render('event', {
-            title: 'conception events',
-            data: JSON.stringify(event),
-            artists: JSON.stringify(artists),
-						event_id: id
-          });
-        }, 0);
 
-      }
     });
 
-  });
 
-
-  return router;
+    return router;
 };
