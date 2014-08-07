caab65c096d74cede2cf1892151dc1cc194e4173
parkerproject <parkerproject@gmail.com>
2014-08-07 03:32:22 +0000

    updates

--- routes/event.js
+++ routes/event.js
@@ -3,12 +3,12 @@ require('dotenv').load();
 var db = require('../config/database');
 
 function getEvent(id, fn) {
-    db.events.findOne({
-        event_id: id
-    }, function(err, event) {
-        if (err || !event) console.log(err);
-        else fn(event);
-    });
+  db.events.findOne({
+    event_id: id
+  }, function(err, event) {
+    if (err || !event) console.log(err);
+    else fn(event);
+  });
 }
 
 
@@ -16,29 +16,28 @@ function getEvent(id, fn) {
 module.exports = function(router) {
 
 
-    router.get('/event/:id', function(req, res) {
-        var id = parseInt(req.params.id);
-        db.artists.find({
-            "events": id
-        }, function(err, artists) {
-            if (err || !artists) {
-                console.log(err);
-            } else {
-                getEvent(id, function(event) {
-                    res.render('event', {
-                        title: 'conception events',
-                        data: JSON.stringify(event),
-                        artists: artists,
-                        event_id: id
-                    });
-                })
-
-
-            }
+  router.get('/event/:id', function(req, res) {
+    var id = parseInt(req.params.id);
+    db.artists.find({
+      "events": id,
+      approved: true
+    }, function(err, artists) {
+      if (err || !artists) {
+        console.log(err);
+      } else {
+        getEvent(id, function(event) {
+          res.render('event', {
+            title: 'conception events',
+            data: JSON.stringify(event),
+            artists: artists,
+            event_id: id
+          });
         });
-
+      }
     });
 
+  });
 
-    return router;
+
+  return router;
 };
