9e964b890d3202cb76140fa5641257e1e46a02f9
parkerproject <parkerproject@gmail.com>
2015-08-11 05:51:12 +0000

    updates

--- routes/whats_new.js
+++ routes/whats_new.js
@@ -1,14 +1,14 @@
 require('dotenv').load();
+var _request = require('request');
+var url = 'https://www.googleapis.com/blogger/v3/blogs/7805519445622295465/posts?key=AIzaSyCLea6TYussNWrxZ7TjTpPWhNcgigrFurc';
 
 
 module.exports = function(router) {
 
   router.get('/what-new', function(req, res) {
 
-    var request = require('request');
-    var api_key = 'AIzaSyCLea6TYussNWrxZ7TjTpPWhNcgigrFurc';
-    var url = 'https://www.googleapis.com/blogger/v3/blogs/7805519445622295465/posts?key=' + api_key;
-    request(url, function(error, response, body) {
+
+    _request(url, function(error, response, body) {
       if (!error && response.statusCode == 200) {
         var blogs = JSON.parse(body);
 
