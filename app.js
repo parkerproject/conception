f278b9d043ab9352f182f097e97fb96cc8f2ec2d
parkerproject <parkerproject@gmail.com>
2015-08-11 03:35:51 +0000

    updated whats new page

--- app.js
+++ app.js
@@ -5,21 +5,21 @@
  */
 
 var express = require('express'),
-    path = require('path'),
-    hbs = require('express-hbs'),
-    logger = require('morgan'),
-    bodyParser = require('body-parser'),
-    compress = require('compression'),
-    favicon = require('static-favicon'),
-    methodOverride = require('method-override'),
-    errorHandler = require('errorhandler'),
-    config = require('./config'),
-    passport = require('passport'),
-    flash = require('connect-flash'),
-    session = require('express-session'),
-    cookieParser = require('cookie-parser'),
-    multer = require('multer'),
-    routes = require('./routes');
+  path = require('path'),
+  hbs = require('express-hbs'),
+  logger = require('morgan'),
+  bodyParser = require('body-parser'),
+  compress = require('compression'),
+  favicon = require('static-favicon'),
+  methodOverride = require('method-override'),
+  errorHandler = require('errorhandler'),
+  config = require('./config'),
+  passport = require('passport'),
+  flash = require('connect-flash'),
+  session = require('express-session'),
+  cookieParser = require('cookie-parser'),
+  multer = require('multer'),
+  routes = require('./routes');
 
 
 var app = express();
@@ -35,11 +35,24 @@ var qt = require('quickthumb');
  * For more information, check out this gist: https://gist.github.com/pheuter/3515945
  */
 hbs.registerHelper('ifvalue', function(conditional, options) {
-    if (options.hash.value === conditional) {
-        return options.fn(this);
-    } else {
-        return options.inverse(this);
-    }
+  if (options.hash.value === conditional) {
+    return options.fn(this);
+  } else {
+    return options.inverse(this);
+  }
+});
+
+hbs.registerHelper("formatDate", function(date) {
+  // This guard is needed to support Blog Posts without date
+  // the takeway point is that custom helpers parameters must be present on the context used to render the templates
+  // or JS error will be launched
+  if (typeof(date) == "undefined") {
+    return "Unknown";
+  }
+	
+	var _date = new Date(date);
+  // These methods need to return a String
+  return _date.getUTCMonth() + "/" + _date.getFullYear();
 });
 
 
@@ -55,37 +68,37 @@ app.set('view engine', 'hbs');
 
 
 app
-    .use(favicon(__dirname + '/public/favicon.ico'))
-    .use(compress())
-    .use(logger('dev'))
-    .use(cookieParser())
-    .use(bodyParser())
-    .use(methodOverride())
-    .use('/artists_images', qt.static(process.env.OPENSHIFT_DATA_DIR + '/artists_images'))
-    .use(express.static(path.join(__dirname, 'public')))
-    .use(multer({
-        dest: process.env.OPENSHIFT_DATA_DIR + '/artists_images'
-    }))
-    .use(session({
-        secret: 'keyboard cat',
-        saveUninitialized: true,
-        resave: true
-    }))
-    .use(passport.initialize())
-    .use(passport.session())
-    .use(flash())
-    .use(routes)
-    .use(function(req, res) {
-        res.status(404).render('404', {
-            title: 'Not Found :('
-        });
+  .use(favicon(__dirname + '/public/favicon.ico'))
+  .use(compress())
+  .use(logger('dev'))
+  .use(cookieParser())
+  .use(bodyParser())
+  .use(methodOverride())
+  .use('/artists_images', qt.static(process.env.OPENSHIFT_DATA_DIR + '/artists_images'))
+  .use(express.static(path.join(__dirname, 'public')))
+  .use(multer({
+    dest: process.env.OPENSHIFT_DATA_DIR + '/artists_images'
+  }))
+  .use(session({
+    secret: 'keyboard cat',
+    saveUninitialized: true,
+    resave: true
+  }))
+  .use(passport.initialize())
+  .use(passport.session())
+  .use(flash())
+  .use(routes)
+  .use(function(req, res) {
+    res.status(404).render('404', {
+      title: 'Not Found :('
     });
+  });
 
 
 if (app.get('env') === 'development') {
-    app.use(errorHandler());
+  app.use(errorHandler());
 }
 
 app.listen(app.get('port'), app.get('ipaddress'), function() {
-    console.log('Express server listening on port ' + app.get('port'));
+  console.log('Express server listening on port ' + app.get('port'));
 });
--- routes/whats_new.js
+++ routes/whats_new.js
@@ -1,13 +1,28 @@
 require('dotenv').load();
+var rp = require('request-promise');
+var api_key = 'AIzaSyCLea6TYussNWrxZ7TjTpPWhNcgigrFurc';
+var url = 'https://www.googleapis.com/blogger/v3/blogs/7805519445622295465/posts?key=' + api_key;
 
 module.exports = function(router) {
 
-    router.get('/what-new', function(req, res) {
+  router.get('/what-new', function(req, res) {
+
+    rp(url)
+      .then(function(response) {
+			
+			var blogs = JSON.parse(response);
+			
+			
+
         res.render('whats_new', {
-            data: 'parker'
+          blogs: blogs.items
         });
-    });
 
 
-    return router;
+      }).catch (console.error);
+
+  });
+
+
+  return router;
 };
