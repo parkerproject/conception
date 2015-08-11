a8e4abb0834d5d897bb5fdab66599c4c2f6d9b6b
parkerproject <parkerproject@gmail.com>
2015-08-11 05:57:51 +0000

    updates

--- routes/whats_new.js
+++ routes/whats_new.js
@@ -1,5 +1,5 @@
 require('dotenv').load();
-var _request = require('request');
+//var _request = require('request');
 var url = 'https://www.googleapis.com/blogger/v3/blogs/7805519445622295465/posts?key=AIzaSyCLea6TYussNWrxZ7TjTpPWhNcgigrFurc';
 
 
@@ -7,16 +7,19 @@ module.exports = function(router) {
 
   router.get('/what-new', function(req, res) {
 
-
-    _request(url, function(error, response, body) {
-      if (!error && response.statusCode == 200) {
-        var blogs = JSON.parse(body);
-
-        res.render('whats_new', {
-          blogs: blogs.items
-        });
-      }
+    res.render('whats_new', {
+      blogs: ''
     });
+
+    //     _request(url, function(error, response, body) {
+    //       if (!error && response.statusCode == 200) {
+    //         var blogs = JSON.parse(body);
+
+    //         res.render('whats_new', {
+    //           blogs: blogs.items
+    //         });
+    //       }
+    //     });
 
 
   });
