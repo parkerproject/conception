f22ae199905051151e9d80cf77faa35cdc2fbfef
Parker <parkerproject@gmail.com>
2016-08-02 17:25:24 -0400

    updates

--- routes/watcher.js
+++ routes/watcher.js
@@ -1,14 +1,14 @@
 'use strict'
 
-const chokidar = require('chokidar')
-const uploader = require('./amazon')
+var chokidar = require('chokidar')
+var uploader = require('./amazon')
 
-let watcher = chokidar.watch(appRoot + '/public/artists_images/', {})
+var watcher = chokidar.watch(appRoot + '/public/artists_images/', {})
 
 watcher
   .on('add', function (path) {
-    let fileArr = path.split('/')
-    let file = fileArr[fileArr.length - 1]
+    var fileArr = path.split('/')
+    var file = fileArr[fileArr.length - 1]
     uploader(file)
     console.log('File', file, 'has been added')
   })
